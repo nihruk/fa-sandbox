1da01616008fccb0049b7c1cd76c9728af5b61d1
pm1v21 <me.pawel.morawski@gmail.com>
2023-06-16 15:58:40 +0100

    Add basic routing and pages.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,25 @@
-export default function Header() {
-  return <div>Header</div>;
+import Link from 'next/link';
+
+function Header() {
+  return (
+    <>
+      <p>NIHR Funding and Awards</p>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/awards-and-outputs">Awards and Outputs</Link>
+        </li>
+        <li>
+          <Link href="#">Apply for funding</Link>
+        </li>
+        <li>
+          <Link href="#">Contact and feedback</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
+
+export default Header;
---
+++ src/pages/awards-and-outputs/award/[id].tsx
@@ -0,0 +1,24 @@
+import { type NextPage } from 'next';
+import Head from 'next/head';
+
+import SearchBar from '~/components/search-bar';
+
+const AwardDetails: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Award title | NIHR Funding and Awards</title>
+        <meta name="description" content="Award Summary" />
+      </Head>
+      <SearchBar />
+      <h3>Award Details</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto illo voluptatibus iure modi
+        consequatur consectetur laudantium autem molestiae assumenda. Obcaecati beatae assumenda
+        accusantium dignissimos ea voluptatum asperiores quod quas dolore?
+      </p>
+    </>
+  );
+};
+
+export default AwardDetails;
