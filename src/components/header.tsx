477ec7db793675ffebeb8a19924087dc19059fad
pm1v21 <me.pawel.morawski@gmail.com>
2023-06-16 15:51:30 +0100

    Update main head

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,20 +2,23 @@ import Link from 'next/link';
 
 function Header() {
   return (
-    <ul>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/awards-and-outputs">Awards and Outputs</Link>
-      </li>
-      <li>
-        <Link href="#">Apply for funding</Link>
-      </li>
-      <li>
-        <Link href="#">Contact and feedback</Link>
-      </li>
-    </ul>
+    <>
+      <p>NIHR Funding and Awards</p>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/awards-and-outputs">Awards and Outputs</Link>
+        </li>
+        <li>
+          <Link href="#">Apply for funding</Link>
+        </li>
+        <li>
+          <Link href="#">Contact and feedback</Link>
+        </li>
+      </ul>
+    </>
   );
 }
 
