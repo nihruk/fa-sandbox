767e689bd95d73bd447c30e9cdd965561525f81b
Guillermina Lorenzo Martinez <glorenzo2001@gmail.com>
2023-06-21 14:34:44 +0100

    Added html to other sites, updated it to JSX, checked spelling mistakes

--- src/components/explore-other-sites.tsx
+++ src/components/explore-other-sites.tsx
@@ -1,7 +1,80 @@
 function ExploreOtherSites() {
   return (
     <>
-      <h5>Explore Other Sites</h5>
+      <section id="explore-other-sites" className="bg-white">
+        <div className="container overflow-hidden">
+          <h5 className="display-2">Explore our other sites</h5>
+          <div className="row gy-3 gy-sm-3 g-md-4 g-lg-5">
+            <div className="col-lg-4">
+              <div className="wrapper">
+                <h6 className="fw-bold">NIHR</h6>
+                <ul>
+                  <li>
+                    <a href="#">Research funding</a>
+                    <i className="fa-solid fa-up-right-from-square"></i>
+                  </li>
+                  <li>
+                    <a href="#">Career development</a>
+                    <i className="fa-solid fa-up-right-from-square"></i>
+                  </li>
+                  <li>
+                    <a href="#">Infrastructure funding</a>
+                    <i className="fa-solid fa-up-right-from-square"></i>
+                  </li>
+                  <li>
+                    <a href="#">Funding opportunities</a>
+                    <i className="fa-solid fa-up-right-from-square"></i>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-lg-4">
+              <div className="wrapper">
+                <h6 className="fw-bold">NIHR Open Data</h6>
+                <ul>
+                  <li>
+                    <a href="#">About HIHR Open Data</a>
+                    <i className="fa-solid fa-up-right-from-square"></i>
+                  </li>
+                  <li>
+                    <a href="#">NIHR Data</a>
+                    <i className="fa-solid fa-up-right-from-square"></i>
+                  </li>
+                  <li>
+                    <a href="#">NIHR Awards</a>
+                    <i className="fa-solid fa-up-right-from-square"></i>
+                  </li>
+                  <li>
+                    <a href="#">NIHR Maps</a>
+                    <i className="fa-solid fa-up-right-from-square"></i>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-lg-4">
+              <div className="wrapper">
+                <h6 className="fw-bold">Journals Library</h6>
+                <ul>
+                  <li>
+                    <a href="#">Five open access journals</a>
+                    <i className="fa-solid fa-up-right-from-square"></i>
+                  </li>
+                  <li>
+                    <a href="#">For authors</a>
+                    <i className="fa-solid fa-up-right-from-square"></i>
+                  </li>
+                  <li>
+                    <a href="#">For reviewers</a>
+                    <i className="fa-solid fa-up-right-from-square"></i>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
