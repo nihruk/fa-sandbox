c6a90150d5d4322a9bb1fa99c68d09fca3b74c1a
Pawel Morawski <me.pawel.morawski@gmail.com>
2023-06-21 15:44:48 +0100

    Update work progress

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,7 +13,7 @@ function Header() {
                 title="National Institute for Health and Care Research (NIHR)"
                 className="main-logo">
                 <Image
-                  src="national-institute-for-health-and-care-research-nihr-logo.svg"
+                  src="/national-institute-for-health-and-care-research-nihr-logo.svg"
                   alt="National Institute for Health and Care Research (NIHR)"
                   width={400}
                   height={40}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <link
-        rel="stylesheet"
-        href="https://unpkg.com/@nihruk/design-system@latest/dist/design-system.bundle.css"
-      />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/@nihruk/design-system@latest/dist/design-system.bundle.css"
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
+        <Script src="https://unpkg.com/@nihruk/design-system@latest/dist/design-system.bundle.js"></Script>
       </body>
     </Html>
   );
