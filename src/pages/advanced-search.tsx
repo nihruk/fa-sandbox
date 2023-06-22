b80e5a363cdd3b88373c0af52eb0c5f8fde4a978
GuillerminaLorenzoMartinez <134301485+GuillerminaLorenzoMartinez@users.noreply.github.com>
2023-06-22 15:49:57 +0100

    Pages (#39)

---
+++ src/pages/advanced-search.tsx
@@ -0,0 +1,23 @@
+import { type NextPage } from 'next';
+import Head from 'next/head';
+
+const AdvancedSearch: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Advanced Search | NIHR Funding and Awards</title>
+        <meta name="description" content="Advanced Search form" />
+      </Head>
+      <div className="container">
+        <h1>Advanced Search</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto illo voluptatibus iure modi
+          consequatur consectetur laudantium autem molestiae assumenda. Obcaecati beatae assumenda
+          accusantium dignissimos ea voluptatum asperiores quod quas dolore?
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default AdvancedSearch;
---
+++ src/pages/contact-and-feedback.tsx
@@ -0,0 +1,23 @@
+import { type NextPage } from 'next';
+import Head from 'next/head';
+
+const ContactAndFeedback: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Contact and Feedback | NIHR Funding and Awards</title>
+        <meta name="description" content="Contact and Feedback form" />
+      </Head>
+      <div className="container">
+        <h1>Contact and Feedback</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto illo voluptatibus iure modi
+          consequatur consectetur laudantium autem molestiae assumenda. Obcaecati beatae assumenda
+          accusantium dignissimos ea voluptatum asperiores quod quas dolore?
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default ContactAndFeedback;
