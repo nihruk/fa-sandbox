13f41f693d9e08de5694a6ab4d7f3e84b2dd458f
GuillerminaLorenzoMartinez <g.lorenzo-martinez@soton.ac.uk>
2023-06-22 13:01:14 +0100

    Updated components/layout, moved search-bar component to components/layout, updated layout component with new path

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,7 +2,7 @@ import Header from '~/components/ui/header';
 import Footer from '~/components/ui/footer';
 import ExploreOtherSites from './explore-other-sites';
 import MainNavigation from './main-navigation';
-import SearchBar from '../search-bar';
+import SearchBar from './search-bar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
