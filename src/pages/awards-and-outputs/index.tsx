310d673585596d0b368824ecd15d530a4fb99294
Pawel Morawski <me.pawel.morawski@gmail.com>
2023-06-29 09:37:36 +0100

    Disable Awards component temporarily

--- src/pages/awards-and-outputs/index.tsx
+++ src/pages/awards-and-outputs/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { type NextPage } from 'next';
 import Head from 'next/head';
 
-import Awards from '~/components/awards/awards';
+//import Awards from '~/components/awards/awards';
 import Outputs from '~/components/outputs/outputs';
+
+// const dummyAwards = [
+//   { id: 1, award_title: 'Award 1' },
+//   { id: 2, award_title: 'Award 2' },
+//   { id: 3, award_title: 'Award 3' }
+// ];
 
 const AwardsAndOutputsPage: NextPage = () => {
   return (
@@ -13,7 +19,6 @@ const AwardsAndOutputsPage: NextPage = () => {
         <meta name="description" content="" />
       </Head>
       <div className="container">
-        <Awards />
         <Outputs />
       </div>
     </>
