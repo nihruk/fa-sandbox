b37d85a83ea96a8d82629a3bf2d8b3a4b6f425ed
Pawel Morawski <me.pawel.morawski@gmail.com>
2023-07-27 17:40:27 +0100

    Update search context

--- src/context/search-context.tsx
+++ src/context/search-context.tsx
@@ -2,22 +2,32 @@ import { createContext, useContext, useReducer } from 'react';
 
 import { useRouter } from 'next/router';
 
+import { type Status } from '~/types';
+
 type SearchStateType = {
   placeholder: string;
   text: string;
+  query: string;
+  status: Status;
 };
 
 type SearchContextType = {
   placeholder: string;
   text: string;
+  query: string;
+  status: Status;
   updateText: (text: string) => void;
   clearText: () => void;
-  submitQuery: (e: React.FormEvent<HTMLFormElement>) => void;
+  submitQueryHandler: (e: React.FormEvent<HTMLFormElement>) => void;
+  statusHandler: (status: Status) => void;
 };
-type SUBMIT = { type: 'SUBMIT' };
-type CLEAR = { type: 'CLEAR' };
-type UPDATE = { type: 'UPDATE'; text: string };
-type Actions = UPDATE | CLEAR | SUBMIT;
+
+type STATUS = { type: 'STATUS'; status: Status };
+type SUBMIT = { type: 'SUBMIT'; text: string; status: Status; query: string };
+type UPDATE = { type: 'UPDATE'; text: string; status: Status };
+type CLEAR = { type: 'CLEAR'; status: Status };
+
+type Actions = UPDATE | CLEAR | STATUS | SUBMIT;
 
 export const SearchContext = createContext<SearchContextType | null>(null);
 
@@ -33,15 +43,31 @@ export function useSearchState() {
 
 function searchReducer(state: SearchStateType, action: Actions) {
   switch (action.type) {
+    case 'STATUS': {
+      return {
+        ...state,
+        status: action.status
+      };
+    }
+    case 'SUBMIT': {
+      return {
+        ...state,
+        query: action.query,
+        status: action.status
+      };
+    }
+
     case 'UPDATE': {
       return {
         ...state,
+        status: action.status,
         text: action.text
       };
     }
     case 'CLEAR': {
       return {
         ...state,
+        status: action.status,
         text: ''
       };
     }
@@ -55,31 +81,49 @@ export function SearchStateProvider({ children }: { children: React.ReactNode })
   const router = useRouter();
   const [state, dispatch] = useReducer(searchReducer, initialSearchState);
 
-  const clearTextFieldHandler = () => {
-    dispatch({
-      type: 'CLEAR'
-    });
+  const statusHandler = (status: Status) => {
+    // dispatch({
+    //   type: 'STATUS',
+    //   status: status
+    // });
+    console.log('statusHandler', status);
   };
 
   const updateSearchFieldHandler = (text: string) => {
+    if (text.length > 0) {
+      dispatch({
+        type: 'UPDATE',
+        text: text,
+        status: 'typing'
+      });
+    } else {
+      clearTextFieldHandler();
+    }
+  };
+
+  const clearTextFieldHandler = () => {
     dispatch({
-      type: 'UPDATE',
-      text: text
+      type: 'CLEAR',
+      status: 'clear'
     });
   };
 
   const submitQueryHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log('submitting query');
-    await router.push(`/awards-and-outputs?text=${state.text}`);
+    dispatch({ type: 'SUBMIT', text: state.text, status: 'submitting', query: state.text });
+    await router.push(`/awards-and-outputs/test?text=${state.text}`);
+    dispatch({ type: 'STATUS', status: 'success' });
   };
 
   const serachContext = {
     placeholder: state.placeholder,
     text: state.text,
+    query: state.query,
+    status: state.status,
     clearText: clearTextFieldHandler,
     updateText: updateSearchFieldHandler,
-    submitQuery: submitQueryHandler
+    submitQueryHandler: submitQueryHandler,
+    statusHandler: statusHandler
   };
 
   return <SearchContext.Provider value={serachContext}>{children}</SearchContext.Provider>;
@@ -88,7 +132,10 @@ export function SearchStateProvider({ children }: { children: React.ReactNode })
 const initialSearchState: SearchContextType = {
   placeholder: 'Search for awards and outputs',
   text: '',
+  query: '',
+  status: 'clear',
   clearText: () => undefined,
   updateText: () => undefined,
-  submitQuery: () => undefined
+  submitQueryHandler: () => undefined,
+  statusHandler: () => undefined
 };
