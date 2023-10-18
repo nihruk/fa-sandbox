21074a959b65118e3958b3fef638878c8c397bd7
Guillermina Lorenzo Martinez <glm1a23@soton.ac.uk>
2023-10-18 11:30:58 +0100

    Updated homepage test and latest awards className

--- cypress/e2e/homepage.cy.ts
+++ cypress/e2e/homepage.cy.ts
@@ -1,5 +1,13 @@
 describe("Home Page", () => {
   it("displays the home page", () => {
+    // Visit homepage
     cy.visit("http://localhost:3000");
+
+    // Latest awards component is loaded with data(6 items)
+    cy.get("h3").should("contain", "Latest Awards");
+
+    cy.contains("h3", "Latest Awards").should("exist");
+
+    cy.get(".latest-awards-component").find(".row").should("have.length", 6);
   });
 });
